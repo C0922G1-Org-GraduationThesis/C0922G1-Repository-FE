08d9b6fce7b226a9f0bb62666c177f76d89ca433
phanvanhung1995 <phanvanhung1224@gmail.com>
2023-04-02 16:36:31 +0700

    hungpv up load source code

--- FE/src/app/service/teacher.service.ts
+++ FE/src/app/service/teacher.service.ts
@@ -6,21 +6,28 @@ import {TeacherDto} from '../dto/teacher-dto';
 @Injectable({
   providedIn: 'root'
 })
-export class TeacherService {
+export class
+TeacherService {
 
   constructor(private httpClient: HttpClient) {
   }
-
+  // <!--create by :HungPV-->
+  // <!--date create : 02/04/2023-->
+  // <!--screen : get all teacher service -->
   getTeacher(name: string, page: number): Observable<Page<TeacherDto>> {
     return this.httpClient.get<Page<TeacherDto>>('http://localhost:8080/api/teachers/list?name=' + name + '&page=' + page);
   }
-
+  // <!--create by :HungPV-->
+  // <!--date create : 02/04/2023-->
+  // <!--screen : delete teacher by id service-->
   deleteTeacherById(idDelete: number): Observable<TeacherDto> {
     return this.httpClient.delete('http://localhost:8080/api/teachers/delete/' + idDelete);
   }
 
 }
-
+// <!--create by :HungPV-->
+// <!--date create : 02/04/2023-->
+// <!--screen : interface paging-->
 export interface Page<T> {
   content: T[];
   pageable: {
